47d6d1ee1e90016cfdcb2d8fe2ebbd759cc5ef15
Ibrahim Khan <116390484+IbrahimKhan2004@users.noreply.github.com>
2025-08-18 14:07:20 +0530

    Update forward.py

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -4,24 +4,23 @@ logger = logging.getLogger(__name__)
 import asyncio
 from pyrogram import filters
 from bot import channelforward
-from config import Config 
+from config import Config
 
 lock = asyncio.Lock()
 
 @channelforward.on_message(filters.channel)
 async def forward(client, message):
-    # Forwarding the messages to the channel
+    # Forwarding only video and sticker messages to the channel
     try:
         for id in Config.CHANNEL:
             from_channel, to_channel = id.split(":")
             if message.chat.id == int(from_channel):
-                if message.video or message.sticker: 
+                if message.video or message.sticker:
                     async with lock:
                         await asyncio.sleep(3)
                         await message.copy(int(to_channel))
                         print("Forwarded a video or sticker from", from_channel, "to", to_channel)
-                else:
-                    await message.copy(int(to_channel))
+                # Removed the else block: other message types will no longer be forwarded
         await asyncio.sleep(10)
         await asyncio.sleep(15)
     except Exception as e:
