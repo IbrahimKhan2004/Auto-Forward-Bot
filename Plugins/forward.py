f226b6026207b7387b4eec24a30e9b57fec3b224
Ibrahim Khan <116390484+IbrahimKhan2004@users.noreply.github.com>
2025-09-12 17:28:05 +0530

    Update forward.py

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -16,14 +16,14 @@ async def forward(client, message):
             from_channel, to_channel = id.split(":")
             if message.chat.id == int(from_channel):
                 if message.video or message.sticker:
-                    await asyncio.sleep(6)
+                    await asyncio.sleep(12)
                     async with lock:
-                        await asyncio.sleep(6)
+                        await asyncio.sleep(12)
                         await message.copy(int(to_channel))
                         print("Forwarded a video or sticker from", from_channel, "to", to_channel)
                     await asyncio.sleep(3)
                 # Removed the else block: other message types will no longer be forwarded
         await asyncio.sleep(10)
-        await asyncio.sleep(15)
+        await asyncio.sleep(10)
     except Exception as e:
         logger.exception(e)
