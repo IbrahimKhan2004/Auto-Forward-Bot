e8800c39985a8079c1063600d75b5a3b4b53eea4
Ibrahim Khan <116390484+IbrahimKhan2004@users.noreply.github.com>
2025-10-19 13:49:47 +0530

    Update forward.py

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -1,9 +1,14 @@
 import logging
 logger = logging.getLogger(__name__)
 
+import asyncio
 from pyrogram import filters
-from bot import channelforward, message_queue
+from pyrogram.errors import FloodWait
+from bot import channelforward
 from config import Config
+
+lock = asyncio.Lock()
+message_queue = []
 
 @channelforward.on_message(filters.channel)
 async def forward(client, message):
@@ -12,7 +17,20 @@ async def forward(client, message):
             from_channel, to_channel = id.split(":")
             if message.chat.id == int(from_channel):
                 if message.video or message.sticker:
-                    await message_queue.put((message, to_channel))
-                    print("Added a message to the queue from", from_channel)
+                    async with lock:
+                        message_queue.append((message, to_channel))
+                        if len(message_queue) == 1:
+                            while message_queue:
+                                msg, dest = message_queue[0]
+                                try:
+                                    await msg.copy(int(dest))
+                                    print("Forwarded in order from", from_channel, "to", dest)
+                                    message_queue.pop(0)
+                                    await asyncio.sleep(3)
+                                except FloodWait as e:
+                                    await asyncio.sleep(e.value * 1.2)
+                                    continue
+                                except Exception:
+                                    pass
     except Exception as e:
         logger.exception(e)
